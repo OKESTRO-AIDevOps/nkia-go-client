f5864549c07386ba6b9c9d27ca9ccf15addfe818
seantywork <seantywork@gmail.com>
2023-07-05 09:45:46 +0900

    basic communication logic and modules

---
+++ goclient/definition.go
@@ -0,0 +1,18 @@
+package goclient
+
+import (
+	"github.com/OKESTRO-AIDevOps/npia-api/pkg/apistandard"
+)
+
+var SESSION_SYM_KEY = ""
+
+var COMM_URL = "/api/v0alpha/test"
+
+var COMM_URL_AUTH = "/auth-challenge/test"
+
+var COMM_URL_MULTIMODE = "/multimode/v0alpha/test"
+
+var ASgi_CliRef = apistandard.ASgi
+
+// challenge_id : ASK, ANS
+// response     : NOPE
--- goclient/test.go
+++ goclient/test.go
@@ -17,17 +17,6 @@ import (
 	goya "github.com/goccy/go-yaml"
 )
 
-var SESSION_SYM_KEY = ""
-
-var COMM_URL = "/api/v0alpha/test"
-
-var COMM_URL_AUTH = "/auth-challenge/test"
-
-var COMM_URL_MULTIMODE = "/multimode/v0alpha/test"
-
-// challenge_id : ASK, ANS
-// response     : NOPE
-
 func LoadConfigYaml_Test() {
 
 	var config_yaml map[string]string
@@ -73,7 +62,7 @@ func BaseFlow_API_Test() {
 		Jar: jar,
 	}
 
-	SESSION_SYM_KEY, err = ClientAuthChallenge(client)
+	err = ClientAuthChallenge(client)
 
 	if err != nil {
 		fmt.Println(err)
@@ -175,7 +164,7 @@ func BaseFlow_APIThenMultiMode_Test() {
 		Jar: jar,
 	}
 
-	SESSION_SYM_KEY, err = ClientAuthChallenge(client)
+	err = ClientAuthChallenge(client)
 
 	if err != nil {
 		fmt.Println(err)
@@ -399,182 +388,4 @@ func BaseFlow_APIThenMultiMode_Test() {
 
 	fmt.Println(string(result_b))
 
-}
-
-func ClientAuthChallenge(client *http.Client) (string, error) {
-
-	var session_sym_key string
-
-	var kube_config map[interface{}]interface{}
-
-	var req_body controller.AuthChallenge
-	var resp_body controller.AuthChallenge
-
-	req_challenge_records := make(modules.ChallengRecord)
-
-	client_context_map := make(map[string]string)
-
-	kube_config_path, err := modules.GetKubeConfigPath()
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	kube_config_file_byte, err := os.ReadFile(kube_config_path)
-
-	err = goya.Unmarshal(kube_config_file_byte, &kube_config)
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	contexts_len := len(kube_config["contexts"].([]interface{}))
-
-	for i := 0; i < contexts_len; i++ {
-
-		context_nm := kube_config["contexts"].([]interface{})[i].(map[string]interface{})["name"].(string)
-
-		pubkey_b, err := modules.GetContextClusterPublicKeyBytes(context_nm)
-
-		if err != nil {
-			return "", fmt.Errorf("chal: %s", err.Error())
-		}
-
-		client_context_map[context_nm] = string(pubkey_b)
-
-	}
-
-	req_challenge_records["ask_challenge"] = client_context_map
-
-	req_body.ChallengeID = "ASK"
-
-	req_body.ChallengeData = req_challenge_records
-
-	req_b, err := json.Marshal(req_body)
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	resp, err := client.Post(COMM_URL_AUTH, "application/json", bytes.NewBuffer(req_b))
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	defer resp.Body.Close()
-
-	body_bytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	err = json.Unmarshal(body_bytes, &resp_body)
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-	challenge_map := resp_body.ChallengeData
-
-	for chal_id, content := range challenge_map {
-
-		for context, enc := range content {
-
-			priv_key_b, err := modules.GetContextUserPrivateKeyBytes(context)
-
-			if err != nil {
-				return "", fmt.Errorf("chal: %s", err.Error())
-			}
-
-			priv_key, err := modules.BytesToPrivateKey(priv_key_b)
-
-			if err != nil {
-				return "", fmt.Errorf("chal: %s", err.Error())
-			}
-
-			enc_b, err := hex.DecodeString(enc)
-
-			if err != nil {
-				return "", fmt.Errorf("chal: %s", err.Error())
-			}
-
-			dec_b, err := modules.DecryptWithPrivateKey(enc_b, priv_key)
-
-			if err != nil {
-				return "", fmt.Errorf("chal: %s", err.Error())
-			}
-
-			dec := string(dec_b)
-
-			challenge_map[chal_id][context] = dec
-
-		}
-
-	}
-
-	req_body.ChallengeID = "ANS"
-
-	req_body.ChallengeData = challenge_map
-
-	req_b, err = json.Marshal(req_body)
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	resp, err = client.Post(COMM_URL_AUTH, "application/json", bytes.NewBuffer(req_b))
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	defer resp.Body.Close()
-
-	body_bytes, err = io.ReadAll(resp.Body)
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	err = json.Unmarshal(body_bytes, &resp_body)
-
-	if err != nil {
-		return "", fmt.Errorf("chal: %s", err.Error())
-	}
-
-	key_records := resp_body.ChallengeKey
-
-	for context, enc := range key_records {
-
-		priv_b, err := modules.GetContextUserPrivateKeyBytes(context)
-
-		if err != nil {
-			return "", fmt.Errorf("chal: %s", err.Error())
-		}
-
-		priv_key, err := modules.BytesToPrivateKey(priv_b)
-
-		if err != nil {
-			return "", fmt.Errorf("chal: %s", err.Error())
-		}
-
-		enc_b, err := hex.DecodeString(enc)
-
-		if err != nil {
-			return "", fmt.Errorf("chal: %s", err.Error())
-		}
-
-		dec_b, err := modules.DecryptWithPrivateKey(enc_b, priv_key)
-
-		if err != nil {
-			return "", fmt.Errorf("chal: %s", err.Error())
-		}
-
-		dec := string(dec_b)
-
-		session_sym_key = dec
-
-	}
-
-	return session_sym_key, nil
 }
